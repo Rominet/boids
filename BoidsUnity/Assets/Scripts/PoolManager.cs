5cb13e856a01e845a95578bf11e15d33c56795bd
Rominet <thierry.destribats@gmail.com>
2014-03-21 15:11:43 +0100

    [Upd] First Boid draft

--- BoidsUnity/Assets/Scripts/PoolManager.cs
+++ BoidsUnity/Assets/Scripts/PoolManager.cs
@@ -11,32 +11,41 @@ public class PoolManager : MonoBehaviour {
 	 * SERIALIZEFIELD *
 	 ******************/
 	[SerializeField]
-	private int countBoids;
+	private int _countBoids;
 	public int CountBoids {
 		get {
-			return countBoids;
+			return _countBoids;
 		}
 		set {
-			countBoids = value;
+			_countBoids = value;
 		}
 	}
 
+    [SerializeField]
+    private float _initVelocityFactor;
+    public float InitVelocityFactor
+    {
+        get { return _initVelocityFactor; }
+        set { _initVelocityFactor = value; }
+    }
+
 	[SerializeField]
-	private GameObject boidsObject;
+	private GameObject _boidsObject;
 	public GameObject BoidsObject
 	{
-		get { return boidsObject; }
-		set { boidsObject = value; }
+		get { return _boidsObject; }
+		set { _boidsObject = value; }
 	}
 
 	[SerializeField]
-	private List<GameObject> obstacleList;
+	private List<GameObject> _obstacleList;
 	public List<GameObject> ObstacleList
 	{
-		get { return boidsObject; }
-		set { boidsObject = value; }
+        get { return _obstacleList; }
+        set { _obstacleList = value; }
 	}
-	/**********
+
+    /**********
 	 * PUBLIC *
 	 **********/
 	private static readonly Vector3 SPAWN_LOCATION = new Vector3(0, 0, 0);
@@ -54,10 +63,11 @@ public class PoolManager : MonoBehaviour {
 
 		//Pool of Boid
 		genePool = new List<GameObject>();
-		for (int i = 0; i < countBoids; ++i) {
+		for (int i = 0; i < _countBoids; ++i) {
 			Vector3 loc = new Vector3(i*3, 0, 0);
-			genePool.Add((GameObject)Instantiate(boidsObject, loc, boidsObject.transform.rotation));
-			genePool[i].rigidbody.velocity = new Vector3(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1)).normalized;
+			genePool.Add((GameObject)Instantiate(BoidsObject, loc, BoidsObject.transform.rotation));
+			genePool[i].rigidbody.velocity = new Vector3(Random.Range(0, 100), Random.Range(0, 100), Random.Range(0, 100)).normalized * InitVelocityFactor;
+            Debug.Log(genePool[i].rigidbody.velocity);
 		}
 	}
 	
